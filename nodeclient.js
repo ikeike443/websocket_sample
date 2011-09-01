89de63eb4ce338f6a0063b802477106b0903d2a9
ikeda.t <ikeda.t@shanon.co.jp>
2011-09-01 17:02:55 +0900

    a little mod

--- nodeclient.js
+++ nodeclient.js
@@ -1,10 +1,11 @@
 var client = require('socket.io-client');
 var socket = client.connect('http://localhost:3000');
-//var socket = client.connect('http://7f376dfa.dotcloud.com');
 
 socket.on('connect',function(){
     console.log('yea!!');
-    socket.send('how are you?');
+//    socket.send('how are you? I am from node server side');
+    socket.send('{myname:"ikeda"}');
     socket.disconnect();
     process.exit(0);
 });
+
